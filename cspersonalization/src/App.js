691ae225ad03c970b812a365d6e3f35fc10be23d
Bobbylee Ingalls <ingallsbobbylee@yahoo.com>
2019-02-06 18:56:46 -0600

    Created test shell

---
+++ cspersonalization/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+
+        </div>
+        <div>
+          <div className="main_navbar">
+            <p>Navbar</p>
+          </div>
+          <div className="main_body">
+            <p>Main Body</p>
+          </div>
+          <div className="main_footer">
+            <p>Footer</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
