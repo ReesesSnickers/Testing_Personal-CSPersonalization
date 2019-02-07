87b6911e6cc635b7bd4fd1ed5d609e787d140e39
Bobbylee Ingalls <ingallsbobbylee@yahoo.com>
2019-02-07 13:31:25 -0600

    added additional theme's

--- cspersonalization/src/App.js
+++ cspersonalization/src/App.js
@@ -28,6 +28,8 @@ class App extends Component {
     const colorPallet1 = " #D9DCD6 #3A7CA5 #16425B"
     const colorPallet2 = " #5F5AA2 #355691 #413F54"
     const colorPallet3 = " #98DFAF #DEEFB7 #5FB49C"
+    const colorPallet4 = " #F5EDF0 #886F68 #424C55"
+    const colorPallet5 = " #E1E6E1 #E63B2E #9B1D20"
     return (
       <div>
         <div className="playground_container">
@@ -49,6 +51,18 @@ class App extends Component {
               <div className="playground_coloroption" style={{backgroundColor: "#98DFAF"}} />
               <div className="playground_coloroption" style={{backgroundColor: "#DEEFB7"}} />
               <div className="playground_coloroption" style={{backgroundColor: "#5FB49C"}} />
+            </div>
+            <div className="playground_optioncontainer">
+              <input type="radio" name="color" value={colorPallet4} onChange={this.handleColorChoice} />
+              <div className="playground_coloroption" style={{backgroundColor: "#F5EDF0"}} />
+              <div className="playground_coloroption" style={{backgroundColor: "#886F68"}} />
+              <div className="playground_coloroption" style={{backgroundColor: "#424C55"}} />
+            </div>
+            <div className="playground_optioncontainer">
+              <input type="radio" name="color" value={colorPallet5} onChange={this.handleColorChoice} />
+              <div className="playground_coloroption" style={{backgroundColor: "#E1E6E1"}} />
+              <div className="playground_coloroption" style={{backgroundColor: "#E63B2E"}} />
+              <div className="playground_coloroption" style={{backgroundColor: "#9B1D20"}} />
             </div>
           </div>
           <h1>Theme Playground</h1>
